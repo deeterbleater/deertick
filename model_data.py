dbfbf1b29adca03eb884e8974c6c073892211693
Seirra <sophietheopossum@yandex.ru>
2024-09-28 23:46:27 +0100

    convert model_data to list

--- model_data.py
+++ model_data.py
@@ -3,17 +3,17 @@ import pandas as pd
 
 df = pd.read_csv('model_data.csv')
 
-model = {}
-model_type = {}
-preferred_providers = {}
-providers = {}
+models = []
 
 for _, row in df.iterrows():
-    model_name = row['model_name']
-    model[model_name] = row['model_id']
-    model_type[model_name] = row['model_type']
-    preferred_providers[model_name] = row['preferred_provider']
-    providers[model_name] = row['providers'].split(', ')
+    #store row of data
+    models.append([
+        row['model_name'],
+        row['model_id'],
+        row['model_type'],
+        row['preferred_provider'],
+        row['providers'].split(', ')
+    ])
 
 
 
