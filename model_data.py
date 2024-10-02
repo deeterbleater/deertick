88991e860f22142cfa375e9bc9cd8f02dc8e1b2d
Seirra <sophietheopossum@yandex.ru>
2024-10-02 19:42:12 +0100

    deduplicate list function

--- model_data.py
+++ model_data.py
@@ -24,6 +24,22 @@ for _, row in df.iterrows():
         else:
             providers.append(row_provider)
 
+def list_all():
+    """
+    Display available models and providers.
+
+    This function prints the available models and providers to the console.
+    It helps users understand the options for model and provider selection.
+    """
+    print("\nmodels:\n")
+    for model in models:
+        print(f'"{model[0]}": "{model[1]}",')
+    print("\nproviders:\n")
+    index = 0
+    for x in providers:
+        print(f'{index}. {x}')
+        index += 1
+
 
 
 df = pd.read_csv('samples.csv')
