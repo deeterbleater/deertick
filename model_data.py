dce875c8c0827b8a0114933a6aecfd084a2e1891
Seirra <sophietheopossum@yandex.ru>
2024-10-18 18:12:53 +0100

    move model back into main list
    in the end i don't need to do anything special with this

--- model_data.py
+++ model_data.py
@@ -208,6 +208,7 @@ models = [
     ["lucataco/animate-diff:beecf59c4aee8d81bf04f0381033dfa10dc16e845b4ae00d281e2fa377e48a9f", "animate-diff", 0, "", 0, "", "", "", "", 0, 0, 0, 0, 0, 0, False, "replicate", "video", []],
     ["yorickvp/llava-13b", "llava-13b", 0, "", 0, "", "", "", "", 0, 0, 0, 0, 0, 0, False, "replicate", "image", []],
     ["mattshumer/reflection-70b", "Reflection 70B", 0, "", 0, "", "text->text", "", "", 0, 0, 0, 0, 0, 0, False, "openrouter", "llm", []],
+    ["huggingfaceh4/zephyr-7b-beta:free", "Hugging Face: Zephyr 7B (free)", 1690934400, "Zephyr is a series of language models that are trained to act as helpful assistants. Zephyr-7B-β is the second model in the series, and is a fine-tuned version of [mistralai/Mistral-7B-v0.1](/models/mistralai/mistral-7b-instruct-v0.1) that was trained on a mix of publicly available, synthetic datasets using Direct Preference Optimization (DPO)._These are free, rate-limited endpoints for [Zephyr 7B](/models/huggingfaceh4/zephyr-7b-beta)._", 4096, "", "text->text", "Mistral", "zephyr", 0.0, 0.0, 0.0, 0.0, 4096.0, 2048.0, False, "openrouter", "llm", []],
 ]
 #versions of models with extended context, each line is a list of changed values.
 models_extended = [
@@ -327,10 +328,6 @@ for model in models_nitro:
     new_model[ModelHead.context_length_top_provider.value] = models_nitro[model]
     new_model[ModelHead.max_completion_tokens_top_provider.value] = models_nitro[model]
     models.append(new_model)
-#add final model that breaks conventions by only being available free
-models.append(
-    ["huggingfaceh4/zephyr-7b-beta:free", "Hugging Face: Zephyr 7B (free)", 1690934400, "Zephyr is a series of language models that are trained to act as helpful assistants. Zephyr-7B-β is the second model in the series, and is a fine-tuned version of [mistralai/Mistral-7B-v0.1](/models/mistralai/mistral-7b-instruct-v0.1) that was trained on a mix of publicly available, synthetic datasets using Direct Preference Optimization (DPO)._These are free, rate-limited endpoints for [Zephyr 7B](/models/huggingfaceh4/zephyr-7b-beta)._", 4096, "", "text->text", "Mistral", "zephyr", 0.0, 0.0, 0.0, 0.0, 4096.0, 2048.0, False, "openrouter", "llm", []],
-)
 providers = [
     'openrouter',
     'replicate',
